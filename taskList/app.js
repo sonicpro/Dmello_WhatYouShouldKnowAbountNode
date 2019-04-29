9611c27101955aeabb38a2fb135892852605ae89
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2019-04-29 18:54:19 +0300

    Refactored using Express.js framework

--- taskList/app.js
+++ taskList/app.js
@@ -1,7 +1,9 @@
 /*global require */
 
-var http = require("http"),
-    port = 8081;
-var requestListener = require("requestListener");
-http.createServer(requestListener).listen(8081);
+var express = require("express"),
+    port = 8081,
+    requestListener = require("requestListener"),
+    app = express();
+
+app.use(requestListener).listen(8081);
 console.log("Server is listening on", port);
