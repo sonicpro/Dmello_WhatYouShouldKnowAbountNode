cb8e4c7127d3f751b72a616e44c3957af53a4fe8
Vlad Tsurko <chegnenago@yahoo.com>
2019-05-25 15:05:05 +0300

    Modified in order tu user system-wide mocha.

--- apiService/test/test.js
+++ apiService/test/test.js
@@ -1,9 +1,11 @@
 /*global describe, it, require */
 var chai = require("chai"),
     expect = chai.expect,
+    should = chai.should(),
     request = require("request"),
     config = require("config"),
-    domain = "http://" + config.host + ":" + config.port;
+    domain = "http://" + config.host + ":" + config.port,
+    taskPayload = "Hello world";
 
 describe("---Testing the task list api---", function (){
     it("POST: Task in list", function (done) {
@@ -16,10 +18,16 @@ describe("---Testing the task list api---", function (){
                 "Content-Type": "application/x-www-form-urlencoded"
             },
             form: {
-                task: "Hello world"
+                task: taskPayload
             }
         };
         request.post(options, function(error, response, body) {
+            // "Schould be" conditions.
+            response.statusCode.should.equal(200);
+
+            // Expected conditions.
+            expect(body).to.be.a("string");
+            expect(body).to.include(taskPayload);
             console.log("We got response: ", body);
             done();
         });
